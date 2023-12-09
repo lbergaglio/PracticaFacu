9123f19b235d61fbbd080926dde33beb7ff69ddb
unknown <lbergaglio@yahoo.com.ar>
2023-12-09 19:27:13 -0300

    dyc2

--- src/main/main.java
+++ src/main/main.java
@@ -8,7 +8,14 @@ public class main {
 		System.out.print(Potencia(3,16));
 	}
 	
-	//EJERCICIO 4 DIVIDE Y CONQUISTA
+	//EJERCICIO 5 DIVIDE Y CONQUISTA
+	private static int ElementoEnPosicion(int[] vector, int inicio, int fin) {
+		int medio=(fin+inicio)/2;
+		if(vector[medio]==medio) return medio;
+		else if(medio<vector[medio]) return ElementoEnPosicion(vector,inicio,medio-1);
+		else if(medio>vector[medio]) return ElementoEnPosicion(vector,medio+1,fin);
+		else return 0;
+	}
 	
 	//EJERCICIO 3 DIVIDE Y CONQUISTA
 	private static int Potencia(int valor,int potencia) {
